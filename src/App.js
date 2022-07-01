cdf2ed92086b055b87bd73ff36892c2a3e993a6b
kavya_kunder <kavyakunder7082@gmail.com>
2022-07-01 19:48:45 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import AllRoutes from "./routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <AllRoutes />
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
     </div>
   );
 }
---
+++ src/pages/Explore.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Explore = () => {
+  return (
+    <div>
+      <h1>This is explore Page</h1>
+    </div>
+  );
+};
+
+export default Explore;
