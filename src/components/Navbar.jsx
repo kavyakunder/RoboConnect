974e20653e40bb2432ca45579646c51cdaec8a49
kavya_kunder <kavyakunder7082@gmail.com>
2022-07-01 22:12:58 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <nav class="w-full fixed	flex  justify-between flex-wrap bg-violet-700 p-6">
       <span class="text-white mr-6 font-semibold text-4xl tracking-tight">
-        RoboChat
+        RoboConnect
       </span>
 
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
